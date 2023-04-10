84232776d4f8ac24c36fa7c83f13f55aa632ae2f
YanoRyo <yanoryo7032@gmail.com>
2023-04-10 23:23:47 +0900

    名前と職業テキストの追加

--- app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myprofilejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myprofilejetpackcompose.ui.theme.MyProfileJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +25,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        // 名前
+                        Text(text = "矢野　涼",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        // 職業
+                        Text(text = "職業: Androidエンジニア",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyProfileJetpackComposeTheme {
-        Greeting("Android")
     }
 }
