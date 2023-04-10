b9d9e749be33accd9b3fe8157b2fe548565a8728
YanoRyo <yanoryo7032@gmail.com>
2023-04-10 23:47:16 +0900

    プロフィール画像の追加

--- app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.myprofilejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +29,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(20.dp)
+                    ) {
+                        // プロフィール画像
+                        Image(painter = painterResource(id = R.drawable.profile_image),
+                            contentDescription = "プロフィール",
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(RoundedCornerShape(30.dp))
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
                         // 名前
                         Text(text = "矢野　涼",
                             color = Color.Gray,
