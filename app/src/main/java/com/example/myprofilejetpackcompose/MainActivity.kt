dd24b8d4ff37de4e2fdc4a45bb3ef14ba735fc47
YanoRyo <yanoryo7032@gmail.com>
2023-04-15 15:59:27 +0900

    リファクタリング

--- app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myprofilejetpackcompose/MainActivity.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myprofilejetpackcompose.components.CompanySection
 import com.example.myprofilejetpackcompose.components.DetailSection
-import com.example.myprofilejetpackcompose.components.Label
 import com.example.myprofilejetpackcompose.ui.theme.MyProfileJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
