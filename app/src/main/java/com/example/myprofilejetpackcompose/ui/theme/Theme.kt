98cd1e1d5016b4ac8518ef87a30b21dbb70bb173
YanoRyo <yanoryo7032@gmail.com>
2023-04-15 16:24:22 +0900

    ステータスバーのカスタマイズ

--- app/src/main/java/com/example/myprofilejetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myprofilejetpackcompose/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200, primaryVariant = Purple700, secondary = Teal200
@@ -33,6 +35,11 @@ fun MyProfileJetpackComposeTheme(
     } else {
         LightColorPalette
     }
+
+    rememberSystemUiController().setSystemBarsColor(
+        color = Color.White,
+        darkIcons = true
+    )
 
     MaterialTheme(
         colors = colors, typography = Typography, shapes = Shapes, content = content
